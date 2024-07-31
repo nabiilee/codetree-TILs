9655492b7bdd6b746765298ba965c217278b0173
dotori <132206182+nabiilee@users.noreply.github.com>
2024-07-31 13:13:12 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240731/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    /*
+    실수 값을 갖는 변수의 경우 소수점 자리를 맞춰 출력하는 경우가 대다수
+    cout << fixed : 소수점 자리 표현을 고정하겠다는 뜻
+    cout.precision(n): 소수점 n째 자리까지 표현
+    추의 무게 곱하기 달에서 중력의 ㅂ율
+    실수값은 전부 소숫점 6째짜리
+
+    */
+    int a = 13;
+    double b = 0.165000;
+    
+    cout.precision(6);
+    double sum = a*b;
+    cout << a << " * " << b << " = " << sum;
+    
+    return 0;
+}
