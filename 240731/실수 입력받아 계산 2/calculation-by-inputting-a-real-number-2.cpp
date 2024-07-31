1fd73ed6bd392f49f06a03ea5b880b8757a54d21
dotori <132206182+nabiilee@users.noreply.github.com>
2024-07-31 16:06:15 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240731/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
+++ 240731/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    
+    cout << fixed;
+    cout.precision(2);    
+    
     double a;
     double s;
 
-    cout << fixed;
-    cout.precision(2);    
     s = a + 1.5;
 
     cout << s << endl;
