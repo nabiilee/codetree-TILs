e394947b309dee93a1782343ebf0075de646f0c1
dotori <132206182+nabiilee@users.noreply.github.com>
2024-07-31 13:09:29 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 1;
+    int b = 2;
+    string c = "C";
+
+    cout << a << "->"<< b << "->" << c << endl;
+    
+    return 0;
+}
