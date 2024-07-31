930a354880cb6cb40ede659800ebc24166c0c088
dotori <132206182+nabiilee@users.noreply.github.com>
2024-07-31 13:22:58 +0900

    Title: 소수점 반올림하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a = 25.352;
+    //소수 첫째자리까지 출력
+    cout << fixed;
+    cout.precision(1);
+    cout << a;
+
+    
+    return 0;
+}
