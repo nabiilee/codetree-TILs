255b7975e41337639a07175fde11c7e7983bc497
dotori <132206182+nabiilee@users.noreply.github.com>
2024-07-31 16:03:51 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240731/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double ft;
+    cin >> ft;
+    //ft를 cm로 환산하기
+    double ft_cm;
+    ft_cm = ft * 30.48;
+    cout << ft_cm;
+    
+    return 0;
+}
