7c7ab9167532e19bdda7ea8bb8ba280683997f1d
dotori <132206182+nabiilee@users.noreply.github.com>
2024-07-31 15:56:49 +0900

    Title: 합을 복사하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240731/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    sum = a + b + c;
+
+    a = sum;
+    b = sum;
+    c = sum;
+
+    cout << a << " " << b << " " << c << " " << endl;
+    
+    return 0;
+}
