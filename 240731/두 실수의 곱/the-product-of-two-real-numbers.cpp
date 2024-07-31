ed9f73be87a1f68dd2242d90eef6e4fc49aa86aa
dotori <132206182+nabiilee@users.noreply.github.com>
2024-07-31 13:36:44 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    double a = 5.26;
+    double b = 8.27;
+
+    //두 수의 곱을 소수점 셋째 자리까지만 출력
+    cout << fixed;
+    cout.precision(3);
+
+    double sum = a * b;
+    cout << sum;
+    
+    return 0;
+}
