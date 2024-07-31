f435a2c02b03ace797e1b5498fedb8611dca5db2
dotori <132206182+nabiilee@users.noreply.github.com>
2024-07-31 14:34:50 +0900

    Title: 변수 값 교체하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/변수 값 교체하기 6/replacing-variable-values-6.cpp
+++ 240731/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -6,11 +6,12 @@ int main() {
     int a= 2; 
     int b = 5;
     int temp = b; //b를 임시 변수 temp에 저장
-    a = temp; //a == 5
     b = a; //b == 2
+    a = temp; //a == 5
+
     cout << a << endl;
     cout << b << endl;
-    
+
 
 
     return 0;
