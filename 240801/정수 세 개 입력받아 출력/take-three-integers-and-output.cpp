8d0f009e68bd2e090ba3ddfc7548ebf2ba84d896
dotori <132206182+nabiilee@users.noreply.github.com>
2024-08-01 14:32:30 +0900

    Title: 정수 세 개 입력받아 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240801/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
+++ 240801/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a, b, c;
-    cin >> a >> " " >> b;
+    cin >> a >> b;
     cin >> c;
     cout << a << " " << b << " " << c << endl;
 
