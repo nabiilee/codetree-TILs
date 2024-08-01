fb51c55f4639209ac0a3dbfa627deec47d4de8c2
dotori <132206182+nabiilee@users.noreply.github.com>
2024-08-01 18:36:47 +0900

    Title: 2개의 정수를 서로 더하기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240801/2개의 정수를 서로 더하기/add-two-integers-each-other.cpp
+++ 240801/2개의 정수를 서로 더하기/add-two-integers-each-other.cpp
@@ -5,10 +5,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
     cin >> a >> b;
-
-    int c;
-    b = a + b;
+    
     a = a + b;
+    b = a + b; 
 
     cout << a << " " << b;
     return 0;
