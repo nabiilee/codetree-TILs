9a286178b0b686c779ec826f7aee3f28562e7f11
dotori <132206182+nabiilee@users.noreply.github.com>
2024-08-01 17:18:35 +0900

    Title: 입력받아 연산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240801/입력받아 연산하기/take-input-and-operate.cpp
+++ 240801/입력받아 연산하기/take-input-and-operate.cpp
@@ -4,9 +4,15 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a, b, c, d;
+
+    
+    cin >> a;
+    cin >> b;
+
+    
     c = a + 87;
     d = b % 10;
-    cin >> a >> b;
+
     cout << c << endl;
     cout << d << endl;
     return 0;
