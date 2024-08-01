2c0db11860b7c39c2f5fe09611ee2edb83f7236f
dotori <132206182+nabiilee@users.noreply.github.com>
2024-08-01 15:18:27 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    char d;
+    cin >> a >> d >> b >> d >>c;
+    cout << a << d << c << d << b;
+    return 0;
+}
