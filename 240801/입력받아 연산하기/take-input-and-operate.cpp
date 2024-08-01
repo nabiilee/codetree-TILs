ada2300a2a675b70fb565b65807db54fde46134d
dotori <132206182+nabiilee@users.noreply.github.com>
2024-08-01 17:16:11 +0900

    Title: 입력받아 연산하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d;
+    c = a + 87;
+    d = b % 10;
+    cin >> a >> b;
+    cout << c << endl;
+    cout << d << endl;
+    return 0;
+}
