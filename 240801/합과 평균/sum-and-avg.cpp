5b85160e445f7af24d35522ef05042e2caa3ce0c
dotori <132206182+nabiilee@users.noreply.github.com>
2024-08-01 22:25:03 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240801/합과 평균/sum-and-avg.cpp
+++ 240801/합과 평균/sum-and-avg.cpp
@@ -5,13 +5,14 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
 
+    cin >> a >> b;
+    
     int c;
     c = a + b;
 
     int d;
     d = (double) (a + b) / 2;
 
-    cin >> a >> b;
     cout << c << " " << d;
 
     return 0;
