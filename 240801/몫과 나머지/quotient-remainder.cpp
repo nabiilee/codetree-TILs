f673bf50382a5193c99584a55cc4a925dcb6413b
dotori <132206182+nabiilee@users.noreply.github.com>
2024-08-01 17:12:44 +0900

    Title: 몫과 나머지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/몫과 나머지/quotient-remainder.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    int c, d;
+    c = a / b;
+    d = a % b;
+    cout << c << "..." << d;
+    
+    return 0;
+}
